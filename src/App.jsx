9d879829e53e321190f0e5bb7e28de5c1b0d655b
techtobit <nurthern2016@gmail.com>
2024-03-22 01:37:08 +0600

    tabel data fetch method added with query

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Example from './Table'
 function App() {
+
+  // useEffect(() => {
+  //   fetch('data.json')
+  //     .then((res) => {
+  //       return res.json();
+  //     })
+  //     .then((data) => {
+  //       console.log(data);
+  //       setPhotos(data);
+  //     });
+  // }, []);
 
   return (
     <>
--- src/Table.jsx
+++ src/Table.jsx
@@ -1,97 +1,135 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import {
-    MaterialReactTable,
-    useMaterialReactTable,
+  MaterialReactTable,
+  useMaterialReactTable,
 } from 'material-react-table';
+import { useQuery } from '@tanstack/react-query';
 
-//nested data is ok, see accessorKeys in ColumnDef below
-const data = [
-    {
-        name: {
-            firstName: 'John',
-            lastName: 'Doe',
-        },
-        address: '261 Erdman Ford',
-        city: 'East Daphne',
-        state: 'Kentucky',
+
+
+const datas = [
+  {
+    name: {
+      firstName: 'John',
+      lastName: 'Doe',
     },
-    {
-        name: {
-            firstName: 'Jane',
-            lastName: 'Doe',
-        },
-        address: '769 Dominic Grove',
-        city: 'Columbus',
-        state: 'Ohio',
+    address: '261 Erdman Ford',
+    city: 'East Daphne',
+    state: 'Kentucky',
+  },
+  {
+    name: {
+      firstName: 'Jane',
+      lastName: 'Doe',
     },
-    {
-        name: {
-            firstName: 'Joe',
-            lastName: 'Doe',
-        },
-        address: '566 Brakus Inlet',
-        city: 'South Linda',
-        state: 'West Virginia',
+    address: '769 Dominic Grove',
+    city: 'Columbus',
+    state: 'Ohio',
+  },
+  {
+    name: {
+      firstName: 'Joe',
+      lastName: 'Doe',
     },
-    {
-        name: {
-            firstName: 'Kevin',
-            lastName: 'Vandy',
-        },
-        address: '722 Emie Stream',
-        city: 'Lincoln',
-        state: 'Nebraska',
+    address: '566 Brakus Inlet',
+    city: 'South Linda',
+    state: 'West Virginia',
+  },
+  {
+    name: {
+      firstName: 'Kevin',
+      lastName: 'Vandy',
     },
-    {
-        name: {
-            firstName: 'Joshua',
-            lastName: 'Rolluffs',
-        },
-        address: '32188 Larkin Turnpike',
-        city: 'Charleston',
-        state: 'South Carolina',
+    address: '722 Emie Stream',
+    city: 'Lincoln',
+    state: 'Nebraska',
+  },
+  {
+    name: {
+      firstName: 'Joshua',
+      lastName: 'Rolluffs',
     },
+    address: '32188 Larkin Turnpike',
+    city: 'Charleston',
+    state: 'South Carolina',
+  },
 ];
 
 const Example = () => {
-    //should be memoized or stable
-    const columns = useMemo(
-        () => [
-            {
-                accessorKey: 'name.firstName', //access nested data with dot notation
-                header: 'First Name',
-                size: 150,
-            },
-            {
-                accessorKey: 'name.lastName',
-                header: 'Last Name',
-                size: 150,
-            },
-            {
-                accessorKey: 'address', //normal accessorKey
-                header: 'Address',
-                size: 200,
-            },
-            {
-                accessorKey: 'city',
-                header: 'City',
-                size: 150,
-            },
-            {
-                accessorKey: 'state',
-                header: 'State',
-                size: 150,
-            },
-        ],
-        [],
-    );
 
-    const table = useMaterialReactTable({
-        columns,
-        data, //data must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
-    });
+  const [data, setData] = useState([])
 
-    return <MaterialReactTable table={table} />;
+  // const {
+  //   data: { data = [], meta } = {},
+  // } = useQuery({
+  //   queryFn:() => 
+  //   fetch('data.json')
+  //   .then((res) => res.json()),
+  //   queryKey:['data']
+  // })
+  // console.log(data);
+
+  // const {data:deatils, error} = useQuery({
+  //   queryFn:() => 
+  //   fetch('data.json')
+  //   .then((res) => res.json()),
+  //   queryKey:['details']
+  // })
+  // console.log('fetch : ',deatils);
+
+
+  const {
+    data: items,
+    error
+  } = useQuery({
+    queryFn: () => fetch('data.json').then((res) => res.json()),
+    queryKey: ['items']
+  });
+  
+  if (error) {
+    console.error('Error fetching data:', error);
+  }
+  
+  console.log(items);
+
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: 'name.firstName', //access nested data with dot notation
+        header: 'First Name',
+        size: 150,
+      },
+      {
+        accessorKey: 'name.lastName',
+        header: 'Last Name',
+        size: 150,
+      },
+      {
+        accessorKey: 'address', //normal accessorKey
+        header: 'Address',
+        size: 200,
+      },
+      {
+        accessorKey: 'city',
+        header: 'City',
+        size: 150,
+      },
+      {
+        accessorKey: 'state',
+        header: 'State',
+        size: 150,
+      },
+    ],
+    [],
+  );
+
+
+  const table = useMaterialReactTable({
+    columns,
+    data,
+  });
+
+  return <MaterialReactTable table={table} />;
 };
 
 export default Example;
