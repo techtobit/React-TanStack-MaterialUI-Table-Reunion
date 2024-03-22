7b95face5019db04113a980190b8600e7ede3e69
techtobit <nurthern2016@gmail.com>
2024-03-22 16:01:41 +0600

    react table installed, json file updated, hook data visualize

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,135 +1,54 @@
-import { useMemo, useState } from 'react';
+import { useMemo} from 'react';
 import {
-  MaterialReactTable,
+  MRT_Table,
   useMaterialReactTable,
+  MRT_GlobalFilterTextField,
 } from 'material-react-table';
-import { useQuery } from '@tanstack/react-query';
+import {
+  Box,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
 
-
-
-const datas = [
-  {
-    name: {
-      firstName: 'John',
-      lastName: 'Doe',
-    },
-    address: '261 Erdman Ford',
-    city: 'East Daphne',
-    state: 'Kentucky',
-  },
-  {
-    name: {
-      firstName: 'Jane',
-      lastName: 'Doe',
-    },
-    address: '769 Dominic Grove',
-    city: 'Columbus',
-    state: 'Ohio',
-  },
-  {
-    name: {
-      firstName: 'Joe',
-      lastName: 'Doe',
-    },
-    address: '566 Brakus Inlet',
-    city: 'South Linda',
-    state: 'West Virginia',
-  },
-  {
-    name: {
-      firstName: 'Kevin',
-      lastName: 'Vandy',
-    },
-    address: '722 Emie Stream',
-    city: 'Lincoln',
-    state: 'Nebraska',
-  },
-  {
-    name: {
-      firstName: 'Joshua',
-      lastName: 'Rolluffs',
-    },
-    address: '32188 Larkin Turnpike',
-    city: 'Charleston',
-    state: 'South Carolina',
-  },
-];
+import TableHook from './TableHook';
 
 const Example = () => {
 
-  const [data, setData] = useState([])
-
-  // const {
-  //   data: { data = [], meta } = {},
-  // } = useQuery({
-  //   queryFn:() => 
-  //   fetch('data.json')
-  //   .then((res) => res.json()),
-  //   queryKey:['data']
-  // })
-  // console.log(data);
-
-  // const {data:deatils, error} = useQuery({
-  //   queryFn:() => 
-  //   fetch('data.json')
-  //   .then((res) => res.json()),
-  //   queryKey:['details']
-  // })
-  // console.log('fetch : ',deatils);
-
-
-  const {
-    data: items,
-    error
-  } = useQuery({
-    queryFn: () => fetch('data.json').then((res) => res.json()),
-    queryKey: ['items']
-  });
-  
-  if (error) {
-    console.error('Error fetching data:', error);
-  }
-  
-  console.log(items);
-
-  const columns = useMemo(
-    () => [
-      {
-        accessorKey: 'name.firstName', //access nested data with dot notation
-        header: 'First Name',
-        size: 150,
-      },
-      {
-        accessorKey: 'name.lastName',
-        header: 'Last Name',
-        size: 150,
-      },
-      {
-        accessorKey: 'address', //normal accessorKey
-        header: 'Address',
-        size: 200,
-      },
-      {
-        accessorKey: 'city',
-        header: 'City',
-        size: 150,
-      },
-      {
-        accessorKey: 'state',
-        header: 'State',
-        size: 150,
-      },
-    ],
-    [],
-  );
-
+  const {columns, items, error, isLoading} = TableHook();
 
   const table = useMaterialReactTable({
     columns,
-    data,
+    data:items || true,
+    enableGlobalFilter:true
   });
 
-  return <MaterialReactTable table={table} />;
+  if (isLoading) {
+    return <div>isLoading</div>;
+  }
+
+  return(
+    <Stack sx={{ m: '2rem 0' }}>
+    <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        {/* <MRT_GlobalFilterTextField table={table} /> */}
+        {/* <MRT_TablePagination table={table} /> */}
+        <MRT_Table table={table} />
+      </Box>
+
+    </Stack> 
+  );
+    
 };
 
 export default Example;
---
+++ src/TableHook.jsx
@@ -0,0 +1,69 @@
+
+import { useQuery } from '@tanstack/react-query';
+import { useMemo } from 'react';
+
+
+const TableHook = () => {
+
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: 'id', 
+        header: 'ID',
+        size: 50,
+      },
+      {
+        accessorKey: 'name', 
+        header: 'Name',
+        size: 100,
+      },
+      {
+        accessorKey: 'category', 
+        header: 'Category',
+        size: 100,
+      },
+      {
+        accessorKey: 'subcategory', 
+        header: 'subcategory',
+        size: 100,
+      },
+      {
+        accessorKey: 'createdAt',
+        header: 'Created At',
+        size: 200,
+      },
+      {
+        accessorKey: 'updatedAt',
+        header: 'Updated At',
+        size: 200,
+      },
+      {
+        accessorKey: 'price',
+        header: 'Price',
+        size: 100,
+      },
+      {
+        accessorKey: 'sale_price',
+        header: 'Sale Price',
+        size: 100,
+      },
+    ],
+    [],
+  );
+
+  const {
+    data: items,
+    error, isLoading
+  } = useQuery({
+    queryFn: () => fetch('data.json').then((res) => res.json()),
+    queryKey: ['items']
+  });
+
+  if (error) {
+    console.error('Error fetching data:', error);
+  }
+
+  return { columns,items, error, isLoading }
+}
+
+export default TableHook;
