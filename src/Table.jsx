e6bed042ed0f1d3cce1febcc3c06f3ea69f849f5
techtobit <nurthern2016@gmail.com>
2024-03-22 18:42:44 +0600

    pagenation, top toggles updated

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,9 +1,11 @@
-import { useMemo} from 'react';
+import { useMemo } from 'react';
 import {
   MRT_Table,
   useMaterialReactTable,
+  MaterialReactTable,
   MRT_GlobalFilterTextField,
 } from 'material-react-table';
+
 import {
   Box,
   Stack,
@@ -17,38 +19,61 @@ import {
 } from '@mui/material';
 
 import TableHook from './TableHook';
+import { getCoreRowModel, getPaginationRowModel } from '@tanstack/react-table';
 
 const Example = () => {
 
-  const {columns, items, error, isLoading} = TableHook();
+  const { columns, items, error, isLoading } = TableHook();
 
   const table = useMaterialReactTable({
     columns,
-    data:items || true,
-    enableGlobalFilter:true
+    data: items || true,
+    enableColumnActions: false,
+    enableDensityToggle: false,
+    enableFullScreenToggle: false,
+    muiSearchTextFieldProps: {
+      placeholder: 'Search all users',
+      sx: { minWidth: '300px' },
+      variant: 'outlined',
+    },
+    muiPaginationProps: {
+      color: 'primary',
+      shape: 'rounded',
+      showRowsPerPage: false,
+      variant: 'outlined',
+    },
+    paginationDisplayMode: 'pages',
+    enableGlobalFilterModes:true,
+
+
   });
 
   if (isLoading) {
     return <div>isLoading</div>;
   }
 
-  return(
+  return (
     <Stack sx={{ m: '2rem 0' }}>
-    <Box
+      <Box
         sx={{
           display: 'flex',
           justifyContent: 'space-between',
           alignItems: 'center',
         }}
       >
-        {/* <MRT_GlobalFilterTextField table={table} /> */}
-        {/* <MRT_TablePagination table={table} /> */}
-        <MRT_Table table={table} />
+    <MaterialReactTable
+    table={table}
+    enableGlobalFilterModes
+      initialState={{
+        showGlobalFilter: true,
+      }}
+      positionGlobalFilter="left"
+      />
       </Box>
 
-    </Stack> 
+    </Stack>
   );
-    
+
 };
 
 export default Example;
